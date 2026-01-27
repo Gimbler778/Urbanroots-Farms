dc154f1854cc708c2f384e4a1db555f3596155d5
Gimbler778 <decoder160@gmail.com>
2026-01-27 23:19:57 +0530

    navbar edited

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,6 +14,10 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
